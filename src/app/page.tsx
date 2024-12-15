bdfe2b857abbd640714697181521bb20eea612a5
inul74 <zmusthofa74@gmail.com>
2024-12-16 00:27:30 +0700

    authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,41 @@
-export default function Home() {
+import { auth, signIn, signOut } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div className="">
       <p>Home page</p>
+      {session && (
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button
+            className="border px-4 py-2 bg-in-red text-white rounded-lg"
+            type="submit"
+          >
+            Logout
+          </button>
+        </form>
+      )}
+      {!session && (
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          <button
+            className="border px-4 py-2 bg-in-red text-white rounded-lg"
+            type="submit"
+          >
+            Login with Google
+          </button>
+        </form>
+      )}
     </div>
   );
 }
